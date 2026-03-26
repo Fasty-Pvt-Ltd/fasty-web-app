4e3335a0bce47c79043ade821f94242f9de89012
thisisatulkumar <theatulkumar11@gmail.com>
2026-03-26 14:23:01 +0530

    feat: implement admin layout with sidebar and loading components

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,22 @@
+import React, { CSSProperties } from 'react';
+import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
+import AppSidebar from '@/components/admin/layout/sidebar/AppSidebar';
+
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<SidebarProvider
+			style={
+				{
+					'--sidebar-width': 'calc(var(--spacing) * 72)',
+					'--header-height': 'calc(var(--spacing) * 12)',
+				} as CSSProperties
+			}
+		>
+			<AppSidebar variant="inset" />
+
+			<SidebarInset>{children}</SidebarInset>
+		</SidebarProvider>
+	);
+};
+
+export default AdminLayout;
---
+++ src/components/admin/layout/sidebar/AppSidebar.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import Link from 'next/link';
+import { useUser, SignInButton, SignOutButton } from '@clerk/nextjs';
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	useSidebar,
+} from '@/components/ui/sidebar';
+import { Button } from '@/components/ui/button';
+import MainSidebarGroup from './MainSidebarGroup';
+import Loading from './Loading';
+
+const links = {
+	mainLinks: [
+		{
+			title: 'Inventory',
+			url: '/admin/inventory',
+		},
+	],
+};
+
+const AppSidebar = ({ ...props }: React.ComponentProps<typeof Sidebar>) => {
+	const { isSignedIn, isLoaded } = useUser();
+
+	const { setOpenMobile } = useSidebar();
+
+	return (
+		<Sidebar collapsible="offcanvas" {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton
+							asChild
+							className="data-[slot=sidebar-menu-button]:p-1.5!"
+						>
+							<Link href="/admin/dashboard" onClick={() => setOpenMobile(false)}>
+								<span className="text-base font-semibold">Fasty Admin</span>
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+
+			<SidebarContent>
+				<MainSidebarGroup items={links.mainLinks} />
+			</SidebarContent>
+
+			<SidebarFooter>
+				{isLoaded ? (
+					isSignedIn ? (
+						<SignOutButton>
+							<Button>Logout</Button>
+						</SignOutButton>
+					) : (
+						<SignInButton mode="modal">
+							<Button>Login</Button>
+						</SignInButton>
+					)
+				) : (
+					<Loading />
+				)}
+			</SidebarFooter>
+		</Sidebar>
+	);
+};
+
+export default AppSidebar;
