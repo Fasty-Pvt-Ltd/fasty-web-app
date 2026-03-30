6533095a2c0ff8ad51861e022f9f5d662a50aec8
anurudhagarwal424-cyber <anurudhagarwal424@gmail.com>
2026-03-30 22:05:42 +0530

    build: place order system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body>{children}</body>
+			</html>
+		</ClerkProvider>
 	);
 }
---
+++ src/types/cart.types.ts
@@ -0,0 +1,8 @@
+export interface Item {
+	product_id: string;
+	name: string;
+	image: string;
+	price: number;
+	quantity: number;
+	stock: number;
+}
