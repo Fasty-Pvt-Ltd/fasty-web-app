8acdf620e16cdc29109d1bee75121669ed122848
thisisatulkumar <theatulkumar11@gmail.com>
2026-03-31 14:20:47 +0530

    fix: remove dummy data

--- src/store/cart.store.ts
+++ src/store/cart.store.ts
@@ -2,34 +2,7 @@ import { create } from 'zustand';
 import { Cart, Item } from '../types/cart.types';
 
 const useCartStore = create<Cart>()((set) => ({
-	// items: [],
-	// neeche wala item bas dummy data hai
-	items: [
-		{
-			image: 'https://thumbs.dreamstime.com/b/beautiful-girl-reading-book-22526658.jpg',
-			name: 'Book (only book not girl)',
-			price: 100,
-			product_id: 'dsklfjdslf',
-			quantity: 1,
-			stock: 5,
-		},
-		{
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpiapw0DMCxV-AOBikjdZfelcGuPZrDFRiwA&s',
-			name: 'Pen',
-			price: 250,
-			product_id: 'abc123',
-			quantity: 2,
-			stock: 10,
-		},
-		{
-			image: 'https://backstudiomilan.com/wp-content/uploads/2021/08/autocad13.jpg',
-			name: 'AutoCad',
-			price: 500,
-			product_id: 'xyz789',
-			quantity: 1,
-			stock: 3,
-		},
-	],
+	items: [],
 
 	addItem: (item: Item) =>
 		set((state) => ({
